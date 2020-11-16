f2fbed433c9d819bec2ec1c3685f9b92ea891cf2
midcenturymodem <71801631+midcenturymodem@users.noreply.github.com>
2020-11-15 20:25:22 -0600

    Initial Commit

---
+++ js/mapScript.js
@@ -0,0 +1,321 @@
+let mapActive = true;
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGhhbHBlcm4iLCJhIjoiY2tnemNuemp1MTJlajJybzN2aDVkMWM0cSJ9.saPhdVcafGvIGMLO6k4pnQ';
+
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    center: [-87.62722735829726, 41.83364353791103], // starting position
+    zoom: 17.521500097254677 // starting zoom
+});
+
+const features = [
+    // start of feature
+    {   // boiler plate for each feature
+        'type': 'Feature',
+        // properties of the marker 
+        'properties': { 
+            // title
+            'title': 'One', 
+            // full text description
+            'description': `
+                <strong>This is a Subheader</strong><p>
+                this is placeholder text placeholder text placeholder text</p>`,
+            // audio track
+            'audioFile': './assets/audio/sample.mp3',
+            // marker image if using
+            'mapMarker': './assets/markers/marker1.png',
+            // 360 image
+            'pano360': './assets/360/SAM_100_1137.jpg',
+            // pano 360 links/text
+            'hotSpots': [
+                    {
+                        "pitch": 14.1,
+                        "yaw": 1.5,
+                        "type": "info",
+                        "text": "Baltimore Museum of Art",
+                        "URL": "https://artbma.org/",
+                    },
+                    {
+                        "pitch": -9.4,
+                        "yaw": 222.6,
+                        "type": "info",
+                        "text": "Art Museum Drive"
+                    },
+                    {
+                        "pitch": -0.9,
+                        "yaw": 144.4,
+                        "type": "info",
+                        "text": "North Charles Street"
+                    }
+                ]
+        },
+        // the location on the map
+        'geometry': { 
+            'type': 'Point',
+            'coordinates': [-87.626666667, 41.832777778]
+        }
+    }, // end of feature
+    
+    // start of feature
+    { 'type': 'Feature',
+        'properties': { 
+            'title': 'Two', 
+            'description': `
+                <strong>This is a Subheader</strong><p>
+                this is placeholder text placeholder text placeholder text</p>`,
+            'audioFile': './assets/audio/sample.mp3',
+            'mapMarker': './assets/markers/marker2.png',
+            'pano360': './assets/360/SAM_100_1140.jpg',
+            'hotSpots': [
+                    // {
+                    //     "pitch": 14.1,
+                    //     "yaw": 1.5,
+                    //     "type": "info",
+                    //     "text": "Baltimore Museum of Art",
+                    //     "URL": "https://artbma.org/",
+                    // },
+                    // {
+                    //     "pitch": -9.4,
+                    //     "yaw": 222.6,
+                    //     "type": "info",
+                    //     "text": "Art Museum Drive"
+                    // },
+                    // {
+                    //     "pitch": -0.9,
+                    //     "yaw": 144.4,
+                    //     "type": "info",
+                    //     "text": "North Charles Street"
+                    // }
+                ]
+        },
+        'geometry': { 
+            'type': 'Point',
+            'coordinates': [-87.627262, 41.832983]
+        }
+    },
+   
+    // start of feature
+    {'type': 'Feature',
+        'properties': { 
+            'title': 'Three', 
+            'description': `
+                <strong>This is a Subheader</strong><p>
+                this is placeholder text placeholder text placeholder text</p>`,
+            'audioFile': './assets/audio/sample.mp3',
+            'mapMarker': './assets/markers/marker3.png',
+            'pano360': './assets/360/SAM_100_1157.jpg',
+            'hotSpots': [
+                    // {
+                    //     "pitch": 14.1,
+                    //     "yaw": 1.5,
+                    //     "type": "info",
+                    //     "text": "Baltimore Museum of Art",
+                    //     "URL": "https://artbma.org/",
+                    // },
+                    // {
+                    //     "pitch": -9.4,
+                    //     "yaw": 222.6,
+                    //     "type": "info",
+                    //     "text": "Art Museum Drive"
+                    // },
+                    // {
+                    //     "pitch": -0.9,
+                    //     "yaw": 144.4,
+                    //     "type": "info",
+                    //     "text": "North Charles Street"
+                    // }
+                ]
+        },
+        'geometry': { 
+            'type': 'Point',
+            'coordinates': [ -87.627643, 41.83291]
+        }
+    },
+    
+    // start of feature
+    { 'type': 'Feature',
+        'properties': { 
+            'title': 'Four', 
+            'description': `
+                <strong>This is a Subheader</strong><p>
+                this is placeholder text placeholder text placeholder text</p>`,
+            'audioFile': './assets/audio/sample.mp3',
+            'mapMarker': './assets/markers/marker4.png',
+            'pano360': './assets/360/SAM_100_1151.jpg',
+            'hotSpots': [
+                    // {
+                    //     "pitch": 14.1,
+                    //     "yaw": 1.5,
+                    //     "type": "info",
+                    //     "text": "Baltimore Museum of Art",
+                    //     "URL": "https://artbma.org/",
+                    // },
+                    // {
+                    //     "pitch": -9.4,
+                    //     "yaw": 222.6,
+                    //     "type": "info",
+                    //     "text": "Art Museum Drive"
+                    // },
+                    // {
+                    //     "pitch": -0.9,
+                    //     "yaw": 144.4,
+                    //     "type": "info",
+                    //     "text": "North Charles Street"
+                    // }
+                ]
+        },
+        'geometry': { 
+            'type': 'Point',
+            'coordinates': [-87.627277, 41.833808]
+        }
+    },
+    
+    // start of feature
+    {'type': 'Feature',
+        'properties': {
+            'title': 'Five', 
+            'description': `
+                <strong>This is a Subheader</strong><p>
+                this is placeholder text placeholder text placeholder text</p>`,
+            'audioFile': './assets/audio/sample.mp3',
+            'mapMarker': './assets/markers/marker5.png',
+            'pano360': './assets/360/SAM_100_1136.jpg',
+            'hotSpots': [
+                    // {
+                    //     "pitch": 14.1,
+                    //     "yaw": 1.5,
+                    //     "type": "info",
+                    //     "text": "Baltimore Museum of Art",
+                    //     "URL": "https://artbma.org/",
+                    // },
+                    // {
+                    //     "pitch": -9.4,
+                    //     "yaw": 222.6,
+                    //     "type": "info",
+                    //     "text": "Art Museum Drive"
+                    // },
+                    // {
+                    //     "pitch": -0.9,
+                    //     "yaw": 144.4,
+                    //     "type": "info",
+                    //     "text": "North Charles Street"
+                    // }
+                ]
+        },
+        'geometry': { 
+            'type': 'Point',
+            'coordinates': [-87.626968, 41.834259]
+        }
+    },
+]
+
+const bufferedFeatures = features.map(i => turf.circle(turf.point(i.geometry.coordinates), 20, {units: 'feet'}))
+
+function checkProximity(coords){
+    if (mapActive) {
+        let clickedPt = turf.point([coords[0], coords[1]]);
+        for (let i=0; i<bufferedFeatures.length; ++i) {
+            if (turf.booleanWithin(clickedPt, bufferedFeatures[i])){
+                document.querySelector("#map-content-title").innerText = features[i].properties.title
+                document.querySelector("#map-content-description").innerHTML = features[i].properties.description
+                document.querySelector("#map-audio-content").src = features[i].properties.audioFile
+                document.querySelector("#map-content").classList += ' active'
+                
+                if (window.location.href.includes("click")) {
+                    pannellum.viewer('photo-viewer', {
+                        "type": "equirectangular",
+                        "panorama": features[i].properties.pano360,
+                        "autoLoad": true,
+                            /*
+                        * Uncomment the next line to print the coordinates of mouse clicks
+                        * to the browser's developer console, which makes it much easier
+                        * to figure out where to place hot spots. Always remove it when
+                        * finished, though.
+                        */
+                        // "hotSpotDebug": true,
+                        "hotSpots": features[i].properties.hotSpots
+                    });
+                    setTimeout(() => {
+                        document.querySelector("#photo-viewer").classList += ' active'
+                        document.querySelector("#return-to-map").classList += ' active'
+                    }, 150)
+                } else {}
+
+                mapActive = false;
+
+                break
+            }
+        }
+    }
+}
+
+function returnToMap(){
+    mapActive = true;
+    document.querySelector("#photo-viewer").classList -= ' active'
+    document.querySelector("#map-content").classList -= ' active'
+    document.querySelector("#return-to-map").classList -= ' active'
+
+}
+
+map.on("load", function(){
+    document.getElementById("mainNav").classList += ' navbar-scrolled';
+
+    features.forEach(feature => {
+        map.loadImage(
+            feature.properties.mapMarker,
+                function (error, image) {
+                    if (error) throw error;
+            map.addImage(feature.properties.title, image);
+                    map.addSource(feature.properties.title, {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'Feature',
+                        'geometry': feature.geometry
+                        }
+                    });
+                    map.addLayer({
+                        'id': feature.properties.title,
+                        'type': 'symbol',
+                        'source': feature.properties.title,
+                        'layout': {
+                            'icon-image': feature.properties.title,
+                            'icon-size': [
+                                'interpolate',
+                                ['exponential', 0.5],
+                                ['zoom'],
+                                18,
+                                0.5,
+                                22,
+                                2
+                            ]
+
+                        }
+                    });
+                }
+        );
+    })
+})
+
+if (window.location.href.includes("click")) {
+    map.on('click', function (e) {
+        checkProximity([e.lngLat.lng,e.lngLat.lat])
+        // console.log([e.lngLat.lng,e.lngLat.lat])
+    })
+} else {
+    // Add geolocate control to the map.
+    var geolocate = new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    });
+
+    // Add the control to the map.
+    map.addControl(geolocate, 'bottom-right');
+
+    // Set an event listener that fires
+    // when a geolocate event occurs.
+    geolocate.on('geolocate', function(e) {
+        checkProximity([e.coords.longitude,e.coords.latitude])
+    });
+}
